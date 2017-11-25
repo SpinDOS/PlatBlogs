49a53deaf793d5e201b710eab0fa9fe0904cdb62
Alexander Platonov <alexanderplat757@gmail.com>
2017-11-25 04:19:04 +0300

    Add partial views for posts

---
+++ PlatBlogs/Pages/_Partials/PostView.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PlatBlogs.Data;
+
+namespace PlatBlogs.Pages._Partials
+{
+    public class PostView
+    {
+        public Post Post { get; set; }
+        public bool Liked { get; set; }
+        public int LikesCount { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
