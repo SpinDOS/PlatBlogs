85cb8df2fc6573d031db3aa80f3c17c334835e70
Alexander Platonov <alexanderplat757@gmail.com>
2017-11-25 04:19:43 +0300

    Add partial views for user left menu

---
+++ PlatBlogs/Pages/_Partials/UserLeftMenu.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PlatBlogs.Data;
+
+namespace PlatBlogs.Pages._Partials
+{
+    public class UserLeftMenu
+    {
+        public ApplicationUser User { get; set; }
+        public int PostCount { get; set; }
+        public int FollowingsCount { get; set; }
+        public int FollowersCount { get; set; }
+        public bool? Followed { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
