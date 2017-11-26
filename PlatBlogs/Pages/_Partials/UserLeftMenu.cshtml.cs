b61f8edc2373c98fc35d697b9cad5347007d28a1
Alexander Platonov <alexanderplat757@gmail.com>
2017-11-27 01:23:33 +0300

    Add collecting info about user to left menu on fly

--- PlatBlogs/Pages/_Partials/UserLeftMenu.cshtml.cs
+++ PlatBlogs/Pages/_Partials/UserLeftMenu.cshtml.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using PlatBlogs.Data;
+using PlatBlogs.Extensions;
 
 namespace PlatBlogs.Pages._Partials
 {
     public class UserLeftMenu
     {
+        public UserLeftMenu(ApplicationUser user, IDbConnection conn, ClaimsPrincipal currentUser)
+        {
+            User = user;
+            var currentUserId = DbConnectionExtensions.GetUserIdByName(conn, currentUser.Identity.Name);
+
+            if (user.UserName != currentUser.Identity.Name)
+                Followed = DbConnectionExtensions.CheckFollowing(conn, user.Id, currentUserId);
+
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = $"SELECT COUNT(*) FROM Posts WHERE AuthorId='{user.Id}'";
+                PostCount = (int) cmd.ExecuteScalar();
+            }
+
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = $"SELECT COUNT(*) FROM Followers WHERE FollowedId='{user.Id}'";
+                FollowersCount = (int) cmd.ExecuteScalar();
+            }
+
+            using (var cmd = conn.CreateCommand())
+            {
+                cmd.CommandText = $"SELECT COUNT(*) FROM Followers WHERE FollowerId='{user.Id}'";
+                FollowingsCount = (int) cmd.ExecuteScalar();
+            }
+        }
         public ApplicationUser User { get; set; }
         public int PostCount { get; set; }
         public int FollowingsCount { get; set; }
