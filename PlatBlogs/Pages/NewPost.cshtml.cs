acf4175d06d8d030191dff8c2e1916cc0fc73040
Alexander Platonov <alexanderplat757@gmail.com>
2017-11-27 02:30:27 +0300

    Add new post page

---
+++ PlatBlogs/Pages/NewPost.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PlatBlogs.Data;
+using PlatBlogs.Extensions;
+
+namespace PlatBlogs.Pages
+{
+    public class NewPostModel : PageModel
+    {
+        public NewPostModel(IDbConnection dbConnection) { DbConnection = dbConnection; }
+        public class InputModel
+        {
+            [Required]
+            [StringLength(450, ErrorMessage = "The post text length must be at max {1} characters long.", MinimumLength = 1)]
+            [DataType(DataType.MultilineText)]
+            public string Text { get; set; }
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        private IDbConnection DbConnection { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                using (var cmd = DbConnection.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@text", Input.Text);
+                    cmd.Parameters.AddWithValue("@author", DbConnectionExtensions.GetUserIdByName(DbConnection, User.Identity.Name));
+
+                    cmd.CommandText = "NewPost";
+                    if (cmd.ExecuteNonQuery() == 1)
+                        return RedirectToPage("/Home");
+                    ModelState.AddModelError(string.Empty, "Error writing text to database");
+                }
+            }
+            return Page();
+        }
+    }
+}
