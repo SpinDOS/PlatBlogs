cccd60a40d66853fef60829305c3e7dbc258f447
Alexander Platonov <alexanderplat757@gmail.com>
2017-11-27 13:19:35 +0300

    Rename Users page to User

---
+++ PlatBlogs/Pages/User.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PlatBlogs.Data;
+
+namespace PlatBlogs.Pages
+{
+    public class UserModel : PageModel
+    {
+        public UserModel(UserManager<ApplicationUser> userManager,
+            IDbConnection dbConnection)
+        {
+            DbConnection = dbConnection;
+            UserManager = userManager;
+        }
+
+        public IDbConnection DbConnection { get; set; }
+        public UserManager<ApplicationUser> UserManager { get; set; }
+
+        public IEnumerable<Post> Posts { get; set; }
+
+
+        public async Task<IActionResult> OnGet(string name)
+        {
+            var user = await UserManager.FindByNameAsync(name);
+            if (user == null)
+                return NotFound();
+            ViewData["User"] = user;
+            return Page();
+        }
+    }
+}
