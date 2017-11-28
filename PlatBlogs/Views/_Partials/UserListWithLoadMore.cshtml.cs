f4a3b9200160ad38a8fe4eef0fa41296e0438486
Alexander Platonov <alexanderplat757@gmail.com>
2017-11-28 04:29:35 +0300

    Add views for lists of users

---
+++ PlatBlogs/Views/_Partials/UserListWithLoadMore.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PlatBlogs.Data;
+
+namespace PlatBlogs.Views._Partials
+{
+    public class UserListWithLoadMoreModel
+    {
+        public IList<ApplicationUser> Users { get; set; }
+        public LoadMoreModel LoadMoreModel { get; set; }
+        public string DefaultText { get; set; }
+    }
+}
