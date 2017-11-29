b67ab1df1187e0225661084dc0b42a6c162efd4f
Alexander Platonov <alexanderplat757@gmail.com>
2017-11-30 01:15:18 +0300

    Add creating PostView object from db query

--- PlatBlogs/Views/_Partials/PostView.cshtml.cs
+++ PlatBlogs/Views/_Partials/PostView.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using PlatBlogs.Data;
 using PlatBlogs.Extensions;
@@ -12,27 +13,49 @@ namespace PlatBlogs.Views._Partials
     public class PostView
     {
         public Post Post { get; set; }
+        public string AuthorFullName { get; set; }
+        public string AuthorUserName { get; set; }
+        public bool AuthorPublicProfile { get; set; }
         public bool Liked { get; set; }
         public int LikesCount { get; set; }
-
-        public static async Task<PostView> FromPost(Post post, DbConnection conn, ClaimsPrincipal currentUser)
+        
+        public static async Task<List<PostView>> FromSqlReaderAsync(DbDataReader reader)
         {
-            var result = new PostView() { Post = post };
-            var myId = await conn.GetUserIdByNameAsync(currentUser.Identity.Name);
-
-            using (var cmd = conn.CreateCommand())
+            var result = new List<PostView>();
+            do
             {
-                cmd.CommandText = "SELECT * FROM Likes WHERE " +
-                                  $"LikerId='{myId}' AND LikedUserId='{post.AuthorId}' AND LikedPostId='{post.Id}'";
-                result.Liked = await cmd.ExecuteScalarAsync() != null;
-            }
+                while (await reader.ReadAsync())
+                {
+                    var authorId = reader.GetString(0);
+                    var postId = reader.GetInt32(1);
+                    var postTime = reader.GetDateTime(2);
+                    var message = reader.GetString(3);
+                    var likesCount = reader.GetInt32(4);
+                    var liked = reader.GetInt32(5) == 1;
+                    var authorFullName = reader.GetString(6);
+                    var authorUserName = reader.GetString(7);
+                    var authorPublicProfile = reader.GetBoolean(8);
 
-            using (var cmd = conn.CreateCommand())
-            {
-                cmd.CommandText = $"SELECT COUNT(*) FROM Likes WHERE LikedUserId='{post.AuthorId}' AND LikedPostId='{post.Id}'";
-                result.LikesCount = (int) await cmd.ExecuteScalarAsync();
+                    var post = new Post()
+                    {
+                        AuthorId = authorId,
+                        Id = postId,
+                        DateTime = postTime,
+                        Message = message,
+                    };
+                    var postView = new PostView()
+                    {
+                        Post = post,
+                        LikesCount = likesCount,
+                        Liked = liked,
+                        AuthorFullName = authorFullName,
+                        AuthorUserName = authorUserName,
+                        AuthorPublicProfile = authorPublicProfile,
+                    };
+                    result.Add(postView);
+                }
             }
-
+            while (await reader.NextResultAsync());
             return result;
         }
     }
