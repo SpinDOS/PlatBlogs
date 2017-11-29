490f64baa2ecbb28c4d4f955f448602e4fa23268
Alexander Platonov <alexanderplat757@gmail.com>
2017-11-29 20:31:43 +0300

    Add GetUserPostsFollowersFollowingsCount stored procedure to db

---
+++ PlatBlogs/Data/Migrations/20171129143404_Add GetUserPostsFollowersFollowingsCount stored procedure.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PlatBlogs.Data.Migrations
+{
+    public partial class AddGetUserPostsFollowersFollowingsCountstoredprocedure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+@"CREATE PROCEDURE GetUserPostsFollowersFollowingsCount
+	@userId NVARCHAR(450),
+    @postsCount INT OUTPUT, 
+    @followingsCount INT OUTPUT, 
+    @followersCount INT OUTPUT
+AS
+    SELECT @postsCount = COUNT(*) FROM Posts
+    WHERE AuthorId = @userId;
+    SELECT @followingsCount = COUNT(*) FROM Followers
+    WHERE FollowerId = @userId;
+	SELECT @followersCount = COUNT(*) FROM Followers
+    WHERE FollowedId = @userId;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE GetUserPostsFollowersFollowingsCount;");
+        }
+    }
+}
