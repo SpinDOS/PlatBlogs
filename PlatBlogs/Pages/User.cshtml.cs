aaf93f4cd7a3937b60a5ac034797852a20f75d03
Alexander Platonov <alexanderplat757@gmail.com>
2017-11-30 01:16:08 +0300

    Add user page

--- PlatBlogs/Pages/User.cshtml.cs
+++ PlatBlogs/Pages/User.cshtml.cs
@@ -10,25 +10,67 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using PlatBlogs.Data;
 using PlatBlogs.Extensions;
+using PlatBlogs.Helpers;
+using PlatBlogs.Pages._Partials;
+using PlatBlogs.Views._Partials;
 
 namespace PlatBlogs.Pages
 {
     public class UserModel : PageModel
     {
-        public UserModel(UserManager<ApplicationUser> userManager) { UserManager = userManager; }
+        public UserModel(ApplicationDbContext dbContext) { DbContext = dbContext; }
 
-        public UserManager<ApplicationUser> UserManager { get; set; }
+        public ApplicationDbContext DbContext { get; set; }
 
-        public IEnumerable<Post> Posts { get; set; }
+        public PostListWithLoadMoreModel PostsModel { get; set; } = new PostListWithLoadMoreModel();
+        public ApplicationUser InspectedUser { get; set; }
+        public bool AjaxResult { get; set; }
+        public bool ClosedProfile { get; set; } = false;
 
-
-        public async Task<IActionResult> OnGet(string name)
+        public async Task<IActionResult> OnGetAsync(string name, [FromQuery] int offset = 0, [FromQuery] bool ajax = false)
         {
-            var user = await UserManager.FindByNameAsync(name);
+            var user = await DbContext.Users
+                .FromSql($"SELECT * FROM AspNetUsers WHERE NormalizedUserName={name.ToUpper()}")
+                .FirstOrDefaultAsync();
             if (user == null)
                 return NotFound();
-            ViewData["User"] = user;
+
+            InspectedUser = user;
+
+            AjaxResult = ajax;
+            if (!ajax)
+                PostsModel.DefaultText = user.FullName + " doesn't have post yet";
+
+            var count = PostsPortion;
+            var overflow = OffsetCountResolver.ResolveOffsetCount(ref offset, ref count, ajax);
+
+            using (var conn = DbContext.Database.GetDbConnection())
+            {
+                await conn.OpenAsync();
+                var myId = await conn.GetUserIdByNameAsync(User.Identity.Name);
+
+                if (!user.PublicProfile && !await conn.IsOpenedForViewerAsync(user.Id, myId))
+                {
+                    ClosedProfile = true;
+                    return Page();
+                }
+
+                PostsModel.Posts = await conn.SimpleQueryPosts(myId, where: names => $"WHERE {names.AuthorId}='{user.Id}'", 
+                    offset: offset, count: count);
+
+                PostsModel.MorePostsExist = PostsModel.Posts.Count == count && !overflow;
+                if (PostsModel.MorePostsExist)
+                {
+                    PostsModel.LoadMoreModel = new LoadMoreModel()
+                    {
+                        Action = "/User/" + name,
+                        Offset = offset + count,
+                    };
+                }
+            }
             return Page();
         }
+
+        public static int PostsPortion => 1;
     }
 }
