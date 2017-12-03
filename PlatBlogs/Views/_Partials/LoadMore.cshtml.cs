fbd25188b0c940162b7d39ee30e055e73030e911
Alexander Platonov <alexanderplat757@gmail.com>
2017-12-03 18:31:07 +0300

    Make LoadMore more customizable

--- PlatBlogs/Views/_Partials/LoadMore.cshtml.cs
+++ PlatBlogs/Views/_Partials/LoadMore.cshtml.cs
@@ -9,14 +9,27 @@ namespace PlatBlogs.Views._Partials
 {
     public class LoadMoreModel
     {
+        public LoadMoreModel(string action) { Action = action; }
+
         public string Action { get; set; }
-        public int Offset { get; set; }
+        public string Text { get; set; } = "Load more";
+
+        public int Offset { get; set; } = 0;
 
         private Dictionary<string, string> _additionalFields = null;
-
+        public Dictionary<string, string> AdditionalFields
+        {
+            get => _additionalFields ?? (_additionalFields = new Dictionary<string, string>());
+            set => _additionalFields = value;
+        }
         public bool HasAdditionalFields => _additionalFields != null && _additionalFields.Count > 0;
 
-        public Dictionary<string, string> AdditionalFields 
-            => _additionalFields ?? (_additionalFields = new Dictionary<string, string>());
+        private string _method = "get";
+        public string Method
+        {
+            get => _method;
+            set => _method = value.ToLower();
+        }
+
     }
 }
