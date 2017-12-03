cc6f3a97cb34cd4776a72937bf4d0967392480ca
Alexander Platonov <alexanderplat757@gmail.com>
2017-12-03 18:32:30 +0300

    Replace User- and Post- ListWithLoadMore with single ListWithLoadMore form with IRenderable

--- PlatBlogs/Views/_Partials/ListWithLoadMore.cshtml.cs
+++ PlatBlogs/Views/_Partials/ListWithLoadMore.cshtml.cs
@@ -2,17 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using PlatBlogs.Data;
 
 namespace PlatBlogs.Views._Partials
 {
-    public class UserListWithLoadMoreModel
+    public class ListWithLoadMoreModel
     {
-        public IList<ApplicationUser> Users { get; set; }
+        public IList<IRenderable> Elements { get; set; }
         public string DefaultText { get; set; }
         public LoadMoreModel LoadMoreModel { get; set; }
-        public bool MoreUsersExist { get; set; } = true;
     }
 }
