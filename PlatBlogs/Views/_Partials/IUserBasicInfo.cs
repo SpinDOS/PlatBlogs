7085e914851f093824c0325e1a5dd0189288dc37
Alexander Platonov <alexanderplat757@gmail.com>
2017-12-05 01:28:24 +0300

    Add interface for basic info about user for posts

---
+++ PlatBlogs/Views/_Partials/IUserBasicInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatBlogs.Views._Partials
+{
+    public interface IUserBasicInfo
+    {
+        string FullName { get; }
+        string UserName { get; }
+        bool PublicProfile { get; }
+    }
+
+    public class SimpleUserBasicInfo: IUserBasicInfo {
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public bool PublicProfile { get; set; }
+    }
+}
