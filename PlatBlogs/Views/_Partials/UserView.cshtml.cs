2473a83fd44c87ba2c7f52b60f20407097f2c9fb
Alexander Platonov <alexanderplat757@gmail.com>
2017-12-05 02:12:13 +0300

    Add mapping UserView from db query

--- PlatBlogs/Views/_Partials/UserView.cshtml.cs
+++ PlatBlogs/Views/_Partials/UserView.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Html;
@@ -14,12 +15,14 @@ namespace PlatBlogs.Views._Partials
 
         public UserViewModel(ApplicationUser user)
         {
+            Id = user.Id;
             FullName = user.FullName;
             UserName = user.UserName;
             AvatarPath = user.AvatarPath;
             PublicProfile = user.PublicProfile;
             ShortInfo = user.ShortInfo;
         }
+        public string Id { get; set; }
         public string FullName { get; set; }
         public string UserName { get; set; }
         private string _avatarPath;
@@ -30,6 +33,29 @@ namespace PlatBlogs.Views._Partials
         }
         public bool PublicProfile { get; set; }
         public string ShortInfo { get; set; }
+
+        public static async Task<List<UserViewModel>> FromSqlReaderAsync(DbDataReader reader)
+        {
+            var result = new List<UserViewModel>();
+            do
+            {
+                while (await reader.ReadAsync())
+                {
+                    var userView = new UserViewModel
+                    {
+                        Id = reader.GetString(0),
+                        FullName = reader.GetString(1),
+                        UserName = reader.GetString(2),
+                        AvatarPath = reader.GetValue(3) as string,
+                        PublicProfile = reader.GetBoolean(4),
+                        ShortInfo = reader.GetValue(5) as string,
+                    };
+                    result.Add(userView);
+                }
+            }
+            while (await reader.NextResultAsync());
+            return result;
+        }
 
         public async Task<object> RenderAsync(IHtmlHelper iHtmlHelper) 
             => await iHtmlHelper.PartialAsync("~/Views/_Partials/UserView.cshtml", this);
