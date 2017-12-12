ee5dbe6811c7a5302245583d8ba1618bce79b3c7
Alexander Platonov <alexanderplat757@gmail.com>
2017-12-13 02:28:49 +0300

    Create Layout for unauthorized users

--- PlatBlogs/Pages/Index.cshtml.cs
+++ PlatBlogs/Pages/Index.cshtml.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PlatBlogs.Data;
 
 namespace PlatBlogs.Pages
 {
     public class IndexModel : PageModel
     {
+        public IndexModel(SignInManager<ApplicationUser> signInManager) { _signInManager = signInManager; }
+
+        private SignInManager<ApplicationUser> _signInManager;
         public IActionResult OnGet()
         {
-            if (User.Identity.IsAuthenticated)
-                return LocalRedirect("/Home");
+            if (_signInManager.IsSignedIn(User))
+                return LocalRedirect("/News");
             return Page();
         }
     }
