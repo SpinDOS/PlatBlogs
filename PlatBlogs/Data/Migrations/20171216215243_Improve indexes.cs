e9817b12c80d3021d47c7180fafca62064950e30
Alexander Platonov <alexanderplat757@gmail.com>
2017-12-17 01:08:45 +0300

    Update indexes on db

---
+++ PlatBlogs/Data/Migrations/20171216215243_Improve indexes.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PlatBlogs.Data.Migrations
+{
+    public partial class Improveindexes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+DROP INDEX [IX_Likes_LikedUserId_LikedPostId] ON [dbo].[Likes];
+
+CREATE NONCLUSTERED INDEX [IX_Likes_LikerId] ON [dbo].[Likes]
+(
+	[LikerId] ASC
+);
+");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+DROP INDEX [IX_Likes_LikerId] ON [dbo].[Likes];
+
+CREATE NONCLUSTERED INDEX [IX_Likes_LikedUserId_LikedPostId] ON [dbo].[Likes]
+(
+	[LikedUserId] ASC,
+	[LikedPostId] ASC
+);
+");
+        }
+    }
+}
